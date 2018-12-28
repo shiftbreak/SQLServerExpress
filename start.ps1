beac8b3f05ac6c81df97455a2a84ba113708a89c
ShiftBreak <35903751+shiftbreak@users.noreply.github.com>
2018-12-28 10:53:35 -0500

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,3 +1,29 @@
+# The script sets the sa password and start the SQL Service 
+# Also it attaches additional database from the disk
+# The format for attach_dbs
+
+param(
+[Parameter(Mandatory=$false)]
+[string]$sa_password,
+
+)
+
+
+# start the service
+Write-Verbose "Starting SQL Server"
+start-service MSSQL`$SQLEXPRESS
+
+if($sa_password -ne "_")
+{
+    Write-Verbose "Changing SA login credentials"
+    $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
+    & sqlcmd -Q $sqlcmd
+}
+
+
+Write-Verbose "Started SQL Server."
+
+
 $lastCheck = (Get-Date).AddSeconds(-2) 
 while ($true) 
 { 
